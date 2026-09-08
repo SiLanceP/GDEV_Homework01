215f8ed53348129af5fd482bbcde015ed45f6b27
Colleen Garcia <155245560+Aschente11@users.noreply.github.com>
2026-09-08 13:20:53 +0800

    task 4-5 done (idk if this'll work)

--- Homework 2/main.cpp
+++ Homework 2/main.cpp
@@ -10,10 +10,37 @@ struct Particle {
     Color color;
 };
 
+const int PARTICLE_COUNT = 1000;
+
+void EmitParticle(Particle* particles, int count, Vector2 position, Vector2 direction, float speed, float lifeTime, Color color) {
+    for (int i = 0; i < count; i++) {
+        if (!particles[i].isActive) {
+            particles[i].isActive = true;
+            particles[i].position = position;
+            particles[i].direction = direction;
+            particles[i].speed = speed;
+            particles[i].lifeTime = lifeTime;
+            particles[i].color = color;
+            return;
+        }
+    }
+}
+
 int main() {
     InitWindow(800, 600, "Particle System");
     SetTargetFPS(60);
 
+    Particle* particles = new Particle[PARTICLE_COUNT];
+ 
+    for (int i = 0; i < PARTICLE_COUNT; i++) {
+        particles[i].isActive = false;
+        particles[i].position = { 0.0f, 0.0f };
+        particles[i].direction = { 0.0f, 0.0f };
+        particles[i].speed = 0.0f;
+        particles[i].lifeTime = 0.0f;
+        particles[i].color = WHITE;
+    }
+    
     while (!WindowShouldClose()) {
         float deltaTime = GetFrameTime();
 
@@ -22,6 +49,7 @@ int main() {
         EndDrawing();
     }
 
+    delete[] particles;
     CloseWindow();
 
     return 0;
